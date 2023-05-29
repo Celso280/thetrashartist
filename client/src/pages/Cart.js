fa4d38b840e82c7ea89bacb8a1528707bc61a0e5
Celso280 <777CelsoD@gmail.com>
2023-05-29 21:20:24 +0800

    :memo: added table in my cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,49 +1,82 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from "../context/AuthContext";
-import axios from 'axios'
-import NavBar from '../components/NavBar'
+import { toast } from 'react-toastify';
+import axios from 'axios';
+import NavBar from '../components/NavBar';
 
 function Cart() {
-
-  const [items, setItems] = useState([])
-  const context = useContext(AuthContext)
-
+  const [items, setItems] = useState([]);
+  const [total, setTotal] = useState(0)
+  const context = useContext(AuthContext);
+  
   useEffect(() => {
     getItemsFromCart()
- }, [])
+  }, []);
 
-  const getItemsFromCart = async() => {
-    const response = await axios.get(`/all-cart-items/${context.user.user_id}`)
-    setItems(response.data) 
- }
+  const getItemsFromCart = async () => {
+    const response = await axios.get(`/all-cart-items/${context.user.user_id}`);
+    setItems(response.data);
+    getTotal(response.data)
+  };
 
- console.log('logging items from Cart.js', items);  
+  const getTotal = (arr) => {
+    console.log('logging arr and items', arr, items);
+    const total = arr.reduce((accumulator, object) => {
+      return accumulator + object.price
+    }, 0)
+
+    setTotal(total)
+  }
+  
+  const getUser = async (id) => {
+    await axios.delete(`http://localhost:8000/delete-cart-item/${id}`)
+    toast.success(`Deleted successfully!`)
+    getItemsFromCart()
+  } 
+
   return (
-    
     <div>
-        <NavBar/>  
-        <div>
-          {items.map((item) => (
-            <div className='w-80 h-60'>
-              <table className='w-9/12 m-auto border-2 mt-20'>
-                <tr>
-                  <th>Image</th>
-                  <th>Name</th>
-                  <th>Quantity</th>
-                  <th>Price</th>
-                </tr>
-                <tr>
-                  <td><img src={item.image_upload} alt='' /></td>
-                  <td>{item.art_name}</td>
-                  <td>{item.quantity}</td>
-                  <td>{item.price}</td>
-                </tr>
-              </table>
-            </div> 
-          ))}            
-        </div>
+      <NavBar/>
+      <div className='text-center'>
+        <table className='m-auto mt-10'>
+          <thead className='bg-[#2f2f2f] text-white'>
+            <tr>
+              <th className='px-10 py-1'>Image</th>
+              <th className='px-10 py-1'>Name</th>
+              <th className='px-10 py-1'>Quantity</th>
+              <th className='px-10 py-1'>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item) => (
+              <tr key={item.id}>
+                <td><img className='w-40 m-auto' src={item.image_upload} alt='' /></td>
+                <td>{item.art_name}</td>
+                <td>{item.quantity}</td>
+                <td>{item.price}<button className='w-5 ml-2 rounded-lg bg-rose-600 text-white' onClick={() => getUser(item.cart_id)}>X</button></td>
+              </tr>
+            ))}
+          </tbody>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td className='bg-[#2f2f2f] text-white'>Total</td>
+              <td className='bg-[#2f2f2f] text-white'>{total}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td><button>Checkout</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
